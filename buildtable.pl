ad2d9852dde65457a638509b73b55d385605928e
Noah Johnson <noah.johnson@loupmail.com>
2022-08-03 12:15:39 -0500

    Update buildtable.pl

--- buildtable.pl
+++ buildtable.pl
@@ -14,18 +14,17 @@ buildtable.pl - Build and check the syllabics composition table.
 =head1 DESCRIPTION
 
 This script takes on standard input the syllabics character data table.
-In C<check> mode it prints out an HTML page containing all syllabics
-characters organized in tables, allowing you to check that everything in
-the character data table is correct.  You must provide the path to a
-WOFF font to use for displaying within the tables and the font name used
-in the CSS.
+In C<check> mode it prints out an HTML page containing all characters
+organized in tables, allowing you to check that everything in the
+character data table is correct.  You must provide the path to a WOFF
+font to use for displaying within the tables and the font name used in
+the CSS.
 
 In C<build> mode it prints out a JSON object that maps key strings to
-value strings.  The key strings are composition strings that store one
-or more Ojitype composeable entities.  The value strings contain the
-resulting codepoint encoded as a single-character string.  Key strings
-include strings containing just a vowel entity.  However, key strings do
-not contain any sequence that doesn't end with a vowel.
+value strings.  The key strings are composition strings that store two
+or more Ojitype composeable syllabics entities, sorted in ascending
+value of numeric codepoint.  The value strings contain the resulting
+codepoint encoded as a single-character string.
 
 =head2 Character data table format
 
@@ -33,25 +32,41 @@ The character data table is a plain US-ASCII text file.  Each line is
 either blank or contains a record.  Records are four base-16 digits
 identifying a Unicode codepoint, a space, and then a definition.
 
-Definitions can have multiple formats.  I<Symbol> definitions are an
-asterisk, a space, and then a sequence of letters giving the symbol a
-name.  These definitions are used for special punctuation marks.
+Definitions can have multiple formats.
+
+I<Symbol> definitions are an asterisk, a space, and then a sequence of
+letters and underscores giving the symbol a name.  These definitions are
+used for punctuation marks and special symbols.
+
+I<Ghost> definitions are a tilde, a space, and then a sequence of
+letters and underscores giving the character a name.  These definitions
+are used for characters that are not visible.
 
 I<Eastern> definitions are for eastern finals.  They consist of just one
 of the following consonant characters:
 
-  p t c k m n s S y
+  p t c k m n s S y l r
 
-The capital S means the sh consonant.
+The capital S means the sh consonant.  For C<l> and C<r> this format
+indicates the final form.
 
 I<Western> definitions are for western finals.  They consist of an
-apostrophe followed by one of the eastern final characters.
+apostrophe followed by one of the eastern final characters.  For C<l>
+and C<r> this format indicates the full-height medial form.
+
+I<Alternate> definitions consist of an ASCII double-quote followed by
+one of the following characters:
+
+  y l r
+
+For C<y> this is the high-plus alternate form.  For C<l> and C<r> this
+format indicates the raised medial form.
 
 I<Common> definitions are for finals that are the same in both eastern
 and western styles.  They consist of just one of the following consonant
 characters:
 
-  l r w h
+  w h
 
 I<Syllable> definitions are for symbols that are neither finals nor
 special symbols.  They have the following structure, in the following
@@ -69,6 +84,23 @@ dots to the left and C<u> used for dots to the right.
 Each record must have a unique definition and a unique codepoint.
 
 =cut
+
+# =========
+# Constants
+# =========
+
+# Set that maps missing syllable definitions to value of one.
+#
+# Unicode does not define all possible syllablics syllables.  This set
+# contains the definitions that Unicode is missing.
+#
+my %MISSING_DFN = (
+  'Sru.a' => 1,
+  'Sru.i' => 1,
+  'Sru.o' => 1,
+  'Sru+i' => 1,
+  'Sru+o' => 1
+);
 
 # ==================
 # Program entrypoint
@@ -115,14 +147,17 @@ my %dfn_map;
 
 # Define a map of decimal codepoint values to parsed definitions;
 # parsed definitions are special strings; the first character of the
-# string is P or E or W or C or S, indicating the type (Punctuation,
-# Eastern, Western, Common, or Syllable); for P types, the rest of the
-# string is the name of the punctuation symbol; for E types and W types,
-# the second character is one of the eastern final characters; for C
-# types, the second character is one of the common symbol characters;
-# for S types, second character is eastern final or dot if none, third
-# character is w or u or dot if none, fourth character is + or dot for
-# none, fifth character is vowel
+# string is P or G or E or W or C or A or S, indicating the type
+# (Punctuation, Ghost, Eastern, Western, Common, Alternate, or
+# Syllable); for P types, the rest of the string is the name of the
+# punctuation symbol; for G types, the rest of the string is the name of
+# the ghost character; for E types and W types, the second character is
+# one of the eastern final characters; for C types, the second character
+# is one of the common symbol characters; for A types, the second
+# character is one of the alternate characters; for S types, second
+# character is eastern final or dot if none, third character is w or u
+# or dot if none, fourth character is + or dot for none, fifth character
+# is vowel
 #
 my %code_map;
 
@@ -137,7 +172,7 @@ while (<STDIN>) {
   (not /\A\s*\z/) or next;
   
   # Parse a record and process it
-  if (/\A([0-9a-fA-F]{4}) \* ([A-Za-z]+)\s*\z/) { # ====================
+  if (/\A([0-9a-fA-F]{4}) \* ([A-Za-z_]+)\s*\z/) { # ===================
     # Symbol -- get fields
     my $cpv   = hex($1);
     my $sname = $2;
@@ -155,7 +190,25 @@ while (<STDIN>) {
     $code_map{$cpv} = $dfn;
     $dfn_map{ $dfn} = $cpv;
     
-  } elsif (/\A([0-9a-fA-F]{4}) ([ptckmnsSy])\s*\z/) { # ================
+  } elsif (/\A([0-9a-fA-F]{4}) ~ ([A-Za-z_]+)\s*\z/) { # ===============
+    # Ghost character -- get fields
+    my $cpv   = hex($1);
+    my $sname = $2;
+    
+    # Build the definition string
+    my $dfn = "G$sname";
+    
+    # Check that neither codepoint nor definition already defined
+    (not defined $code_map{$cpv}) or
+      die (sprintf "Codepoint %04x redefined, stopped", $cpv);
+    (not defined $dfn_map{$dfn}) or
+      die "Definition $dfn used twice, stopped";
+    
+    # Add to both maps
+    $code_map{$cpv} = $dfn;
+    $dfn_map{ $dfn} = $cpv;
+    
+  } elsif (/\A([0-9a-fA-F]{4}) ([ptckmnsSylr])\s*\z/) { # ==============
     # Eastern final -- get fields
     my $cpv = hex($1);
     my $fch = $2;
@@ -173,7 +226,7 @@ while (<STDIN>) {
     $code_map{$cpv} = $dfn;
     $dfn_map{ $dfn} = $cpv;
     
-  } elsif (/\A([0-9a-fA-F]{4}) '([ptckmnsSy])\s*\z/) { # ===============
+  } elsif (/\A([0-9a-fA-F]{4}) '([ptckmnsSylr])\s*\z/) { # =============
     # Western final -- get fields
     my $cpv = hex($1);
     my $fch = $2;
@@ -191,7 +244,7 @@ while (<STDIN>) {
     $code_map{$cpv} = $dfn;
     $dfn_map{ $dfn} = $cpv;
   
-  } elsif (/\A([0-9a-fA-F]{4}) ([lrwh])\s*\z/) { # =====================
+  } elsif (/\A([0-9a-fA-F]{4}) ([wh])\s*\z/) { # =======================
     # Common final -- get fields
     my $cpv = hex($1);
     my $fch = $2;
@@ -208,9 +261,27 @@ while (<STDIN>) {
     # Add to both maps
     $code_map{$cpv} = $dfn;
     $dfn_map{ $dfn} = $cpv;
+  
+  } elsif (/\A([0-9a-fA-F]{4}) "([ylr])\s*\z/) { # =====================
+    # Alternate -- get fields
+    my $cpv = hex($1);
+    my $fch = $2;
+    
+    # Build the definition string
+    my $dfn = "A$fch";
+    
+    # Check that neither codepoint nor definition already defined
+    (not defined $code_map{$cpv}) or
+      die (sprintf "Codepoint %04x redefined, stopped", $cpv);
+    (not defined $dfn_map{$dfn}) or
+      die "Definition $dfn used twice, stopped";
+    
+    # Add to both maps
+    $code_map{$cpv} = $dfn;
+    $dfn_map{ $dfn} = $cpv;
     
   } elsif # ============================================================
-    (/\A([0-9a-fA-F]{4}) ([ptckmnsSy])?(w|u)?(\+)?([aeio])?\s*\z/) {
+    (/\A([0-9a-fA-F]{4}) ([ptckmnsSylr])?(w|u)?(\+)?([aeio])?\s*\z/) {
     # Syllable -- get fields
     my $cpv  = hex($1);
     my $cons = $2;
@@ -263,7 +334,7 @@ while (<STDIN>) {
 
 # Make sure all eastern and western finals are defined
 #
-for my $c (split //, "ptckmnsSy") {
+for my $c (split //, "ptckmnsSylr") {
   (defined $dfn_map{"W$c"}) or
     die "Missing western final $c, stopped";
   (defined $dfn_map{"E$c"}) or
@@ -272,14 +343,22 @@ for my $c (split //, "ptckmnsSy") {
 
 # Make sure all common finals are defined
 #
-for my $c (split //, "lrwh") {
+for my $c (split //, "wh") {
   (defined $dfn_map{"C$c"}) or
     die "Missing common final $c, stopped";
 }
 
-# Make sure all syllable combinations are defined
+# Make sure all alternate finals are defined
 #
-for my $initial (split //, ".ptckmnsSy") {
+for my $c (split //, "ylr") {
+  (defined $dfn_map{"A$c"}) or
+    die "Missing alternate final $c, stopped";
+}
+
+# Warn which syllable combinations are missing -- not all combinations
+# are defined by Unicode, so there will be warnings
+#
+for my $initial (split //, ".ptckmnsSylr") {
   for my $wdot (split //, ".wu") {
     for my $vlen (split //, ".+") {
       for my $vowl (split //, "aeio") {
@@ -289,7 +368,10 @@ for my $initial (split //, ".ptckmnsSy") {
         # Build current syllable definition
         my $dfn = "S$initial$wdot$vlen$vowl";
         
-        # Check that syllable definition is present
+        # If syllable is on the missing syllable list, skip it
+        (not defined $MISSING_DFN{$dfn}) or next;
+        
+        # Check that syllable definition is present or warn
         (defined $dfn_map{$dfn}) or
           die "Missing syllable $dfn, stopped";
       }
@@ -362,6 +444,10 @@ th {
   font-weight: bold;
   text-align: center;
 }
+
+.missing {
+  background-color: silver;
+}
     
     </style>
   </head>
@@ -379,7 +465,7 @@ EOF
 
   for my $k (sort keys %dfn_map) {
     # Only proceed if key is for a punctuation symbol
-    if ($k =~ /\AP([A-Za-z]+)\z/) {
+    if ($k =~ /\AP([A-Za-z_]+)\z/) {
       my $pname = $1;
       my $sym   = chr($dfn_map{$k});
       my $cpv   = sprintf "U+%04X", $dfn_map{$k};
@@ -387,6 +473,31 @@ EOF
   <tr>
     <td>$pname</td>
     <td>$sym</td>
+    <td>$cpv</td>
+  </tr>
+EOF
+    }
+  }
+  
+  print "</table>\n";
+  
+  # Write the ghost symbols table
+  print <<EOF;
+<table>
+  <tr>
+    <th>Ghost symbol</th>
+    <th>Codepoint</th>
+  </tr>
+EOF
+
+  for my $k (sort keys %dfn_map) {
+    # Only proceed if key is for a ghost symbol
+    if ($k =~ /\AG([A-Za-z_]+)\z/) {
+      my $pname = $1;
+      my $cpv   = sprintf "U+%04X", $dfn_map{$k};
+      print <<EOF;
+  <tr>
+    <td>$pname</td>
     <td>$cpv</td>
   </tr>
 EOF
@@ -404,7 +515,7 @@ EOF
   </tr>
 EOF
 
-  for my $c (split //, "lrwh") {
+  for my $c (split //, "wh") {
     my $sym = chr($dfn_map{"C$c"});
     my $cpv = sprintf "U+%04X", $dfn_map{"C$c"};
     print <<EOF;
@@ -425,28 +536,51 @@ EOF
     <th>Final</th>
     <th colspan="2">Eastern</th>
     <th colspan="2">Western</th>
+    <th colspan="2">Alternate</th>
   </tr>
 EOF
 
-  for my $c (split //, "ptckmnsSy") {
+  for my $c (split //, "ptckmnsSylr") {
     my $west = chr($dfn_map{"W$c"});
     my $east = chr($dfn_map{"E$c"});
+    my $altc;
     my $wcpv = sprintf "%04X", $dfn_map{"W$c"};
     my $ecpv = sprintf "%04X", $dfn_map{"E$c"};
+    my $acpv;
+    
+    if (($c eq 'y') or ($c eq 'l') or ($c eq 'r')) {
+      $altc = chr($dfn_map{"A$c"});
+      $acpv = sprintf "%04X", $dfn_map{"A$c"};
+    }
     
     if ($c eq 'S') {
       $c = 'sh';
     }
     
-    print <<EOF;
+    if (defined $altc) {
+      print <<EOF;
   <tr>
     <td>$c</td>
     <td>$east</td>
     <td>$ecpv</td>
     <td>$west</td>
     <td>$wcpv</td>
+    <td>$altc</td>
+    <td>$acpv</td>
   </tr>
 EOF
+    } else {
+      print <<EOF;
+  <tr>
+    <td>$c</td>
+    <td>$east</td>
+    <td>$ecpv</td>
+    <td>$west</td>
+    <td>$wcpv</td>
+    <td class="missing" colspan="2">&nbsp;</td>
+  </tr>
+EOF
+    }
   }
   
   print "</table>\n";
@@ -455,7 +589,7 @@ EOF
   for my $wdot (split //, ".wu") {
     # Print appropriate header
     if ($wdot eq '.') {
-      print <<EOF
+      print <<EOF;
 <table>
   <tr>
     <th>Initial</th>
@@ -467,7 +601,7 @@ EOF
 EOF
       
     } else {
-      print <<EOF
+      print <<EOF;
 <table>
   <tr>
     <th>Initial</th>
@@ -480,21 +614,21 @@ EOF
     }
     
     # Print each row
-    for my $initial (split //, ".ptckmnsSy") {
+    for my $initial (split //, ".ptckmnsSylr") {
     
       # Start the row with the initial
       if ($initial eq 'S') {
-        print <<EOF
+        print <<EOF;
   <tr>
     <th>sh</th>
 EOF
       } elsif ($initial eq '.') {
-        print <<EOF
+        print <<EOF;
   <tr>
     <th>&mdash;</th>
 EOF
       } else {
-        print <<EOF
+        print <<EOF;
   <tr>
     <th>$initial</th>
 EOF
@@ -503,17 +637,29 @@ EOF
       # Go through all recognized length and vowel combinations in the
       # same order as in the header
       for my $suf ('.e', '.i', '+i', '.o', '+o', '.a', '+a') {
+        # Get current syllable definition
+        my $sdfn = "S$initial$wdot$suf";
+        
+        # If current syllable definition is missing, then print a
+        # missing cell and skip
+        if (defined $MISSING_DFN{$sdfn}) {
+          print <<EOF;
+    <td class="missing">&nbsp;</td>
+EOF
+          next;
+        }
+        
         # Get the current syllable symbol
-        my $sym = chr($dfn_map{"S$initial$wdot$suf"});
+        my $sym = chr($dfn_map{$sdfn});
         
         # Print the current syllable symbol
-        print <<EOF
+        print <<EOF;
     <td>$sym</td>
 EOF
       }
       
       # Finish the row
-      print <<EOF
+      print <<EOF;
   </tr>
 EOF
     }
@@ -526,15 +672,58 @@ EOF
   print "  </body>\n</html>\n";
   
 } elsif ($mode eq 'build') { # =========================================
+  # Switch to UTF-8 output
+  binmode(STDOUT, ":encoding(UTF-8)") or
+    die "Failed to set UTF-8 output, stopped";
+  
   # Start the JSON object
   print "{";
   
   # Print each record
   my $first_rec = 1;
-  for my $initial (split //, ".ptckmnsSy") {
+  for my $initial (split //, ".ptckmnsSylr") {
     for my $wdot (split //, ".wu") {
       for my $vlen (split //, ".+") {
         for my $vowl (split //, "aeio") {
+          # Skip if this is the illegal e with vowel length
+          (not (($vlen eq '+') and ($vowl eq 'e'))) or next;
+          
+          # Get the definition
+          my $dfn = "S$initial$wdot$vlen$vowl";
+          
+          # Skip if this is one of the missing definitions
+          (not defined $MISSING_DFN{$dfn}) or next;
+          
+          # Get all the component entity codepoints
+          my @ea;
+          
+          if ($initial ne '.') {
+            push @ea, ($dfn_map{"S$initial..a"});
+          }
+          
+          if ($wdot eq 'w') {
+            push @ea, (0x140e);
+            
+          } elsif ($wdot eq 'u') {
+            push @ea, (0x140f);
+          }
+          
+          if ($vlen eq '+') {
+            push @ea, (0x1404);
+          }
+          
+          push @ea, ($dfn_map{"S...$vowl"});
+          
+          # Skip if there are less than two component entities
+          (scalar(@ea) > 1) or next;
+          
+          # Sort in numeric order of codepoint
+          my @eas = sort { $a <=> $b } @ea;
+          
+          # Translate codepoints into individual string characters and
+          # then rejoin into a single string
+          my $key_string = join "", (map chr, @eas);
+          
           # Print appropriate delimiter
           if ($first_rec) {
             print "\n";
@@ -543,45 +732,8 @@ EOF
             print ",\n";
           }
           
-          # Get the appropriate definition
-          my $dfn;
-          if ($vowl eq 'e') {
-            $dfn = "S$initial$wdot.$vowl";
-          } else {
-            $dfn = "S$initial$wdot$vlen$vowl";
-          }
-          
-          # Get the appropriate entity sequence
-          my $eseq = '';
-          
-          if ($initial ne '.') {
-            my $ei = chr($dfn_map{"S$initial..a"});
-            $eseq = $eseq . $ei;
-          }
-          
-          if ($wdot eq 'w') {
-            $eseq = $eseq . "\x{140e}";
-            
-          } elsif ($wdot eq 'u') {
-            $eseq = $eseq . "\x{140f}";
-          }
-          
-          if ($vlen eq '+') {
-            $eseq = $eseq . "\x{1404}";
-          }
-          
-          $eseq = $eseq . chr($dfn_map{"S...$vowl"});
-          
-          # Write escaped entity sequence
-          my $es = '"';
-          for my $c (split //, $eseq) {
-            my $cpv = ord($c);
-            $es = $es . '\u' . sprintf("%04x", $cpv);
-          }
-          $es = $es . '"';
-          
           # Print the mapping
-          printf "%s: \"\\u%04x\"", $es, $dfn_map{$dfn};
+          printf "\"%s\": \"%s\"", $key_string, chr($dfn_map{$dfn});
         }
       }
     }
